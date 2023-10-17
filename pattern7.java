a2cdd3596d80cfb4a6d92e6d281a1561f0e50dba
MONIKAKARDAM <133112537+MONIKAKARDAM@users.noreply.github.com>
2023-10-17 20:56:19 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,15 @@
+public public class pattern7 {
+    
+    
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i;j<5;j++){ 
+                System.out.print("*");
+                }
+            
+            System.out.println();
+           }
+        }
+
+    
+}
